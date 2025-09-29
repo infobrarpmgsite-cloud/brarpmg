624cd754c73d5f96ece0f7c3cbe5734a8af0437a
infobrarpmgsite-cloud <infobrarpmgsite@gmail.com>
2025-09-29 13:52:05 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <div className="flex items-center space-x-2">
             <div className='flex-shrink-0 rounded-full p-5 lg:p-10 bg-white/50'>
               <img 
-                src="/logo.png"
+                src="./logo.png"
                 alt="BPMG STAYS"
                 width={400}
                 height={400}
